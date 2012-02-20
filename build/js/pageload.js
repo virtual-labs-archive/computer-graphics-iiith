b0041fd505bc6080ffbf345a0e7a3dcba49256c9
revanth.nr <revanth.nr@872ac714-7d09-4a26-b260-3fba7b6ef299>
2012-02-20 14:50:11 +0000

    - Testing Manual
    git-svn-id: file:///labs/cse18/svn/CG_Vlabs@55 872ac714-7d09-4a26-b260-3fba7b6ef299

---
+++ build/js/pageload.js
@@ -0,0 +1,23 @@
+function loadPage(pagename)
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = function()
+		{
+			if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+				document.getElementById("page").innerHTML=xmlhttp.responseText;
+			}
+		}
+	xmlhttp.open("GET", pagename, true);
+	xmlhttp.send();
+}
