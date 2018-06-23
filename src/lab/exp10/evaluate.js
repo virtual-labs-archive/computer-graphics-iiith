8e88cf0f308216329eac0306bc6e322ba8f94585
VishwanathamSarika <vishwanathamsarika@gmail.com>
2018-06-23 06:21:22 +0530

    Quizzes are added for the experiment 11-Rasterization:Polygon with evaluate and reset options along with solutions

---
+++ src/lab/exp10/evaluate.js
@@ -0,0 +1,76 @@
+function eval()
+{
+	var form = document.forms["quiz"];
+	var win = window.open("","win","width=500,height=500,scrollbars");
+	win.focus();
+	win.document.open();
+	win.document.write('<title>Solution</title>');
+	win.document.write('<body bgcolor="#FFFFFF">');
+        win.document.write('<center><h3>Score</h3></center>');
+
+	/* Initialise answers */
+	var a1 = "1";
+	var a2 = "2";
+	var a3 = "2";
+	var a4 = "1";
+
+	/* Extract answers */
+	var q1, q2, q3, q4, length;
+	
+	length = form["q1"].length;
+	for(i=0; i<length; i++)
+		if(form["q1"][i].checked)
+			q1 = form["q1"][i].value;
+	
+	length = form["q2"].length;
+	for(i=0; i<length; i++)
+		if(form["q2"][i].checked)
+			q2 = form["q2"][i].value;
+	
+	length = form["q3"].length;
+	for(i=0; i<length; i++)
+		if(form["q3"][i].checked)
+			q3 = form["q3"][i].value;
+
+	length = form["q4"].length;
+	for(i=0; i<length; i++)
+		if(form["q4"][i].checked)
+			q4 = form["q4"][i].value;
+	
+	/* Evaluate answers */
+	var score = 0;
+	var result = "Correct answers: ";
+
+	if(q1 == a1)
+	{
+		score++;
+		result += "Q1, ";
+	}
+
+	if(q2 == a2)
+	{
+		score++;
+		result += "Q2, ";
+	}
+
+	if(q3 == a3)
+	{
+		score++;
+		result += "Q3, ";
+	}
+
+	if(q4 == a4)
+	{
+		score++;
+		result += "Q4";
+	}
+
+	/* Show result */
+	var output = "Your score is " + score + "<br>";
+	output += result;
+	
+	//alert(output);
+	win.document.write(output+"<br>");
+        win.document.write('<center><h3>Solution to Quiz</h3></center>');
+        win.document.write("Ans 1)Option 1"+"<br>"+"Ans 2)Option 2"+"<br>"+"Ans 3)Option 2"+"<br>"+"Ans 4)Option 1");
+}
