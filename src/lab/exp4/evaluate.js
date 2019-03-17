eb978d07a3e636392d870606025ea7bd04129e61
mrinalkhubchandani13 <mrinalkhubchandani13@gmail.com>
2019-03-17 22:56:21 +0530

    Issue fixed in which Q4 was named as Q2

--- src/lab/exp4/evaluate.js
+++ src/lab/exp4/evaluate.js
@@ -29,7 +29,7 @@ function eval()
 	length = form["q4"].length;
 	for(i=0; i<length; i++)
 		if(form["q4"][i].checked)
-			q2 = form["q4"][i].value;
+			q4 = form["q4"][i].value;
 	
 	/* Evaluate answers */
 	var score = 0;
