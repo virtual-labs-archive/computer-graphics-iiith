445de7043ca262a3ebe21aadda62434fc7e27fdf
Shreevignesh Suriyanarayanan <sshreevignesh@gmail.com>
2019-03-29 21:10:00 +0530

    Fixed quiz issue

--- src/lab/exp1/evaluate.js
+++ src/lab/exp1/evaluate.js
@@ -10,6 +10,7 @@ function eval()
 	var a1e = "(4, -3)";
 	var a2  = "1";
 	var a3  = "3";
+	var first_correct_answer_printed = 0;
 
 	/* Extract answers */
 	var q1a = form["q1_a"].value;
@@ -18,66 +19,123 @@ function eval()
 	var q1d = form["q1_d"].value;
 	var q1e = form["q1_e"].value;
 	var q2, q3, length;
-	
+
 	length = form["q2"].length;
 	for(i=0; i<length; i++)
-		if(form["q2"][i].checked)
-			q2 = form["q2"][i].value;
-	
+	if(form["q2"][i].checked)
+	q2 = form["q2"][i].value;
+
 	length = form["q3"].length;
 	for(i=0; i<length; i++)
-		if(form["q3"][i].checked)
-			q3 = form["q3"][i].value;
+	if(form["q3"][i].checked)
+	q3 = form["q3"][i].value;
 
 	/* Evaluate answers */
 	var score = 0;
-	var result = "Correct answers: ";
+	var result = "Your Correct Answers: ";
 
 	if(q1a == a1a)
 	{
 		score++;
-		correct += "Q1a, ";
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+		else
+		{
+			first_correct_answer_printed++;
+		}
+		result += "Q1a";
 	}
 
 	if(q1b == a1b)
 	{
 		score++;
-		result += "Q1b, ";
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+
+		else
+		{
+			first_correct_answer_printed++;
+		}
+		result += "Q1b";
 	}
 
 	if(q1c == a1c)
 	{
 		score++;
-		result += "Q1c, ";
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+		else
+		{
+			first_correct_answer_printed++;
+		}
+		result += "Q1c";
 	}
 
 	if(q1d == a1d)
 	{
 		score++;
-		result += "Q1d, ";
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+		else
+		{
+			first_correct_answer_printed++;
+		}
+		result += "Q1d";
 	}
 
 	if(q1e == a1e)
 	{
 		score++;
-		result += "Q1e, ";
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+		else
+		{
+			first_correct_answer_printed++;
+		}
+		result += "Q1e";
 	}
 
 	if(q2 == a2)
 	{
 		score++;
-		result += "Q2, ";
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+		else
+		{
+			first_correct_answer_printed++;
+		}
+		result += "Q2";
 	}
 
 	if(q3 == a3)
 	{
 		score++;
+		if(first_correct_answer_printed)
+		{
+			result+=", ";
+		}
+		else
+		{
+			first_correct_answer_printed++;
+		}
 		result += "Q3";
 	}
 
 	/* Show result */
 	var output = "Your score is " + score + "\n";
 	output += result;
-	
+
 	alert(output);
 }
